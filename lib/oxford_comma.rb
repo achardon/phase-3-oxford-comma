bcc70a2457f4a17cbb32245c39f0acaa52b94e36
Alessandra Chardon <achardon@ucla.edu>
2021-12-02 16:03:13 -0500

    commit tree 800d479ac5fedba32444199a3bedff26f008003c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        return array[0]
+    elsif array.length == 2
+        return array.join(' and ')
+    else
+        array[-1] = 'and ' + array[-1]
+        return array.join(', ')
+    end
 end
+
+puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
